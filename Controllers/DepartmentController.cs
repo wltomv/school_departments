7b5f79e4d1252c1c5200160ffdef50a01987f9ba
Walter Mach <owaldovelasz@gmail.com>
2023-01-07 20:05:20 -0600

    Add controller and method to list the departments

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using school_departments.Resources;
+using System.Data;
+using System.Text.Json.Serialization;
+
+namespace school_departments.Controllers
+{
+    [ApiController]
+    [Route("department")]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listDepartments")]
+        public dynamic ListDepartments()
+        {
+            DataTable tDepartments = DBData.List("dept.list_departments");
+
+            string deptJSON = JsonConvert.SerializeObject(tDepartments);
+            return new
+            {
+                success = true,
+                message = "success",
+                data = deptJSON
+            };
+        }
+    }
+}
